9e31c585972050839c09ad24ba0ccf84c9d18d73
I-Am-Lain <vetter.william@gmail.com>
2020-09-22 22:42:34 -0500

    F I N A L

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'Female', 'Brown', "Feisty", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 25, 'Male', 'Gold', "Banana-y", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 7, 'Female', 'Pink', "Feisty", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 15, 'Female', 'Orange', "Possibly Dead", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 6, 'Female', 'Red', "Calm", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 9, 'Female', 'Blue', "Mellow", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 12, 'Female', 'Orange', "Ornery", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 50, "Male", "Blood-stained", "At peace", 1);
